96f637a9b754795b0805dbd02e1d521295c0817b
sarthakharne <harnesarthak@gmail.com>
2022-03-25 13:23:32 +0530

    Multiplication

--- Number.cpp
+++ Number.cpp
@@ -6,3 +6,29 @@ Number::Number(vector<int> &dig, int b, int expo, bool sig)  {
     exponent = expo;
     sign = sig;
 }
+
+Number::Number(Number* n) {
+    this->digits = n->digits;
+    this->base = n->base;
+    this->exponent = n->exponent;
+    this->sign = n->sign;
+}
+
+void Number::removeZeroes() {
+    int i = this->digits.size() - 1;
+
+    while(i >= 0) {
+        // remove digit if zero
+        if(this->digits[i] == 0)
+            this->digits.pop_back();
+        else
+            break;
+    }
+
+    this->exponent = this->digits.size();
+}
+
+Number* Number::addExponent(int e) {
+    this->exponent += e;
+    return this;
+}
