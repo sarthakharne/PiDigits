cee4cfdfb7582be1e9336858b41b55d70970aad3
sarthakharne <harnesarthak@gmail.com>
2022-03-28 20:33:36 +0530

    commit

--- Multiplication.cpp
+++ Multiplication.cpp
@@ -71,7 +71,7 @@ Number* Karatsuba(Number* n1, Number* n2) {
     int size1 = n1->digits.size();
     int size2 = n2->digits.size();
 
-    // base case, when only 1 digit is received    
+    // base case, when only 1 digit is received
     if(size1 == 1 || size2 == 1) {
         return NormalMult(n1, n2);
     }
@@ -79,7 +79,7 @@ Number* Karatsuba(Number* n1, Number* n2) {
     /*
         low1 * * * * * * * * * * | * * * * * * * * * * * high1
         low2 * * * * * * * * * * | * * * * *  high2
-        
+
         low1 * * * * * * * * * highspl1 | lowspl1 * * * * * * * * * * high1
         low2 * * * * * * * * * highspl2 | lowspl2 * * * *  high2
     */
@@ -90,26 +90,72 @@ Number* Karatsuba(Number* n1, Number* n2) {
 
     pair<Number*, Number*> n1Split = splitAt(n1, greaterSizBy2);
     pair<Number*, Number*> n2Split = splitAt(n2, greaterSizBy2);
-    
+
     Number *lowSpl1 = new Number(n1Split.first);
     Number *highSpl1 = new Number(n1Split.second);
     Number *lowSpl2 = new Number(n2Split.first);
     Number *highSpl2 = new Number(n2Split.second);
+    // lowSpl1->printNumber();
+    // highSpl1->printNumber();
+    // lowSpl2->printNumber();
+    // highSpl2->printNumber();
 
     // free memory
     free(n1Split.first);
     free(n1Split.second);
     free(n2Split.first);
     free(n2Split.second);
-    
+
     Number* higherMult = Karatsuba(highSpl1, highSpl2);
+    // cout << "highSpl1: ";
+    // highSpl1->printNumber();
+    // cout << "highSpl2: ";
+    // highSpl2->printNumber();
+    // cout << "higherMult: ";
+    // higherMult->printNumber();
     Number* lowerMult = Karatsuba(lowSpl1, lowSpl2);
+    // cout << "lowSpl1: ";
+    // lowSpl1->printNumber();
+    // cout << "lowSpl2: ";
+    // lowSpl2->printNumber();
+    // cout << "lowerMult: ";
+    // lowerMult->printNumber();
 
     Number* Sum1 = Add(lowSpl1, highSpl1);
+    // cout << "lowSpl1: ";
+    // lowSpl1->printNumber();
+    // cout << "highSpl1: ";
+    // highSpl1->printNumber();
+    // cout << "Sum1: ";
+    // Sum1->printNumber();
     Number* Sum2 = Add(lowSpl2, highSpl2);
+    // cout << "lowSpl2: ";
+    // lowSpl2->printNumber();
+    // cout << "highSpl2: ";
+    // highSpl2->printNumber();
+    // cout << "Sum2: ";
+    // Sum2->printNumber();
     Number* Prod1 = Karatsuba(Sum1, Sum2);
+    // cout << "Sum1: ";
+    // Sum1->printNumber();
+    // cout << "Sum2: ";
+    // Sum2->printNumber();
+    // cout << "Prod1: ";
+    // Prod1->printNumber();
     Number* Diff1 = Sub(Prod1, higherMult);
+    // cout << "Prod1: ";
+    // Prod1->printNumber();
+    // cout << "higherMult: ";
+    // higherMult->printNumber();
+    // cout << "Diff1: ";
+    // Diff1->printNumber();
     Number* Diff2 = Sub(Diff1, lowerMult);
+    cout << "Diff1: ";
+    Diff1->printNumber();
+    cout << "lowerMult: ";
+    lowerMult->printNumber();
+    cout << "Diff2: ";
+    Diff2->printNumber();
     Number* highMultPlusLowerMult = Diff2;
 
     higherMult->addExponent(2*greaterSizBy2);
@@ -146,8 +192,9 @@ Number* Multiply(Number* n1, Number* n2) {
 }
 
 int main() {
-    vector<int> a = {1, 4, 7, 4, 4};
-    vector<int> b = {2, 7, 2, 2, 7, 1, 1, 6, 9};
+    // TODO: Correct the code for this input
+    vector<int> a = {2, 1};
+    vector<int> b = {5, 1};
     Number* A = new Number(a, 10, 0, 0);
     Number* B = new Number(b, 10, 0, 0);
     Number* ans = Multiply(A, B);
